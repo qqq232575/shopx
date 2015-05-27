27da215696b47473ecdb51da21663bfd43a80b2b
109171650 <109171650@qq.com>
2015-05-27 17:45:10 +0800

    1

--- shop/control/index.php
+++ shop/control/index.php
@@ -60,8 +60,6 @@ class indexControl extends BaseHomeControl{
 		}
 		Tpl::output('arr_floor',$arr_floor);
 
-		Tpl::output('fixed_search_bar','1');
-
 		Model('seo')->type('index')->show();
 		Tpl::showpage('index');
 	}
