004512b76de116cb2c9c16bd8b0a690aa837b93e
109171650 <109171650@qq.com>
2015-06-02 16:46:55 +0800

    1

--- plugin/zoom/zoom.php
+++ plugin/zoom/zoom.php
@@ -1,10 +1,26 @@
-<?php defined('In_OS') or exit('Access Invalid!');?>
-<link href="/plugin/zoom/css/cloud-zoom.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="/plugin/zoom/js/cloud-zoom.1.0.2.min.js"></script>
+<?php defined('IN_OS') or exit('Access Invalid!');?>
+
+
+	                <?php if (!empty($output['goods_image'])) {?>
+	                <?php echo implode(',', $output['goods_image']);?>
+	                <?php }?>
+<link href="<?php echo BASE_SITE_URL;?>/plugin/zoom/css/cloud-zoom.css" rel="stylesheet" type="text/css" />
+<style>
+.ncs-handle {
+  width: 350px;
+  height: 24px;
+  padding-top: 10px;
+  border-top: dotted 1px #EEE;
+  position: absolute;
+  z-index: 1;
+  left: 10px;
+  top: 422px;
+</style>
+<script type="text/javascript" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/js/cloud-zoom.1.0.2.min.js"></script>
 	<div class="zoom-section">    	  
 		<div class="zoom-small-image">
-			<a href-lcl='/plugin/zoom/images/3.png' class = 'cloud-zoom' id='zoom1' rel="adjustX:10, adjustY:-4">
-			<img src="/plugin/zoom//images/2.png" title="" />
+			<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class = 'cloud-zoom' id='zoom1' rel="adjustX:10, adjustY:-4">
+			<img src="<?php echo BASE_SITE_URL;?>/plugin/zoom//images/2.png" title="" />
 			</a>
 		</div>
 
@@ -12,13 +28,13 @@
 			<span class="left disable">&lt;</span>
 		  <div class="small">
 			<div>		              
-<a href-lcl='/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' /plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="/plugin/zoom/images/1.png" /></a>
-<a href-lcl='/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' /plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="/plugin/zoom/images/1.png" /></a>
-<a href-lcl='/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' /plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="/plugin/zoom/images/1.png" /></a>
-<a href-lcl='/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' /plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="/plugin/zoom/images/1.png" /></a>
-<a href-lcl='/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' /plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="/plugin/zoom/images/1.png" /></a>
-<a href-lcl='/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' /plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="/plugin/zoom/images/1.png" /></a>
-<a href-lcl='/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' /plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="/plugin/zoom/images/1.png" /></a>			
+<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' <?php echo BASE_SITE_URL;?>/plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/images/1.png" /></a>
+<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' <?php echo BASE_SITE_URL;?>/plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/images/1.png" /></a>
+<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' <?php echo BASE_SITE_URL;?>/plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/images/1.png" /></a>
+<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' <?php echo BASE_SITE_URL;?>/plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/images/1.png" /></a>
+<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' <?php echo BASE_SITE_URL;?>/plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/images/1.png" /></a>
+<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' <?php echo BASE_SITE_URL;?>/plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/images/1.png" /></a>
+<a href-lcl='<?php echo BASE_SITE_URL;?>/plugin/zoom/images/3.png' class='cloud-zoom-gallery' title='Blue' rel="useZoom: 'zoom1', smallImage: ' <?php echo BASE_SITE_URL;?>/plugin/zoom/images/2.png'"><img class="zoom-tiny-image" src="<?php echo BASE_SITE_URL;?>/plugin/zoom/images/1.png" /></a>			
 		    </div>
 		  </div>
 			<span class="right">&gt;</span>			
