2c2b6944412f78b518827e9172d055b94bb5badc
109171650 <109171650@qq.com>
2015-06-02 14:59:03 +0800

    1

--- plugin/fixed_search_bar.php
+++ plugin/fixed_search_bar.php
@@ -11,21 +11,27 @@
 }
 </style>
 <script type="text/javascript">
-$(function(){
-		window.onscroll = function(){
-        if(300 < $(document).scrollTop()){
+$(document).ready(function(){
+	//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+	$(function () {
+	$(window).scroll(function(){
+    	if ($(window).scrollTop()>100){
             $('.header-wrap').addClass("change_search_bar").fadeIn('slow');
             $('.public-head-layout ').css({'height':'50px'})
             $('.keyword,.head-app,.head-user-menu,.head-search-bar .tab').css({'display':'none'});
             $('.public-head-layout .site-logo').css({'margin-right':'150px','height':'50px'});
             $('.head-search-bar').css({'padding-top':'13px'});
-        }else{
+    	}
+	else
+    	{
             $('.header-wrap').removeClass("change_search_bar");
             $('.public-head-layout').css({'height':'100px'})
             $('.keyword,.head-app,.head-user-menu,.head-search-bar .tab').css({'display':'block'});
             $('.public-head-layout .site-logo').css({'margin-right':'10px'});
             $('.head-search-bar').css({'padding-top':'28px'});
-        }
-    }
+    	}
+	});
+	});
 });
+
 </script>
