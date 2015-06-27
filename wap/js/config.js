467667b4415dab752ffe27b4111586efecff99c1
Charlie Root <root@FreeBSD.domain>
2015-06-28 02:13:04 +0800

    1

--- wap/js/config.js
+++ wap/js/config.js
@@ -1,5 +1,5 @@
-var SiteUrl = "http://localhost/shopx";//"http://1.shopxv3b8.com";
-var ApiUrl =  "http://localhost/shopx/mobile";//"http://1.shopxv3b8.com/mobile";
+var SiteUrl = "http://192.168.1.168/";//"http://1.shopxv3b8.com";
+var ApiUrl =  "http://192.168.1.168/mobile";//"http://1.shopxv3b8.com/mobile";
 var pagesize = 10;
-var WapSiteUrl ="http://localhost/shopx/wap";//"http://1.shopxv3b8.com/wap";
-var AndroidSiteUrl = "http://localhost/shopx/app.apk";
+var WapSiteUrl ="http://192.168.1.168/wap";//"http://1.shopxv3b8.com/wap";
+var AndroidSiteUrl = "http://192.168.1.168/app.apk";
